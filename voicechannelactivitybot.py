bbe45204ad7fc3959935c6f144534464e0ebcac1
Nathan Mendoza <74679703+nathancmendoza@users.noreply.github.com>
2021-12-23 11:34:14 -0800

    Create a log channel for the bot if it does not already exist

--- voicechannelactivitybot.py
+++ voicechannelactivitybot.py
@@ -14,11 +14,13 @@ class VoiceChannelActivityBot(discord.Client):
             Loads the environment variables from a local .env file
             Then constructs a typical discord client
         """
-
+        
         # load server (guild) name and authentication token
         load_dotenv()
         self.TOKEN = os.getenv('DISCORD_TOKEN')
         self.GUILD = os.getenv('DISCORD_GUILD')
+        self.log = None
+        self.logname = 'vcalog'
 
         # Use the parent constructor to construct the client
         super().__init__()
@@ -30,10 +32,28 @@ class VoiceChannelActivityBot(discord.Client):
             to the server and ready to operate.
         """
         guild = discord.utils.get(self.guilds, name=self.GUILD)
+        if all([c.name != self.logname for c in guild.channels]):
+            self.log = await guild.create_text_channel(self.logname)
+        
         print (
             f'{self.user} is connected to the following guild:\n'
             f'{guild.name}(id: {guild.id})'
         )
+
+    async def handle_voice_channel_join(member):
+        pass
+        
+
+    async def handle_voice_channel_leave(member):
+        pass
+
+
+    async def on_voice_state_update(self, member, before, after):
+        if before.channel is None and after.channel is not None:
+            await handle_voice_channel_join(member)
+        if before.channel is not None and after.channel is None:
+            await handle_voice_channel_leave(member)
+
 
     def run(self):
         """
