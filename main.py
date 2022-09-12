0f2eea3822ff81b75dcc66d7ce9e51fe637aa82f
Nathan Mendoza <74679703+nathancmendoza@users.noreply.github.com>
2022-09-11 23:10:28 -0700

    Address linting issues

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
+"""
+    Entry point for starting up the discord voice channel activity bot
+"""
 from voicechannelactivitybot import VoiceChannelActivityBot
 
 def main():
+    """
+        start the bot
+    """
     VoiceChannelActivityBot().run()
 
 
